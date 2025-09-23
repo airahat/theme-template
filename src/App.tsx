6fd3659700dc79f2c80e9537f69242371084c16e
airahat <airahat1998@gmail.com>
2025-09-23 13:02:09 +0600

    basic templating except js

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Layout from "./components/layout/Layout"
+
+function App() {
+
+  return (
+    <>
+
+      <Layout />
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Dashboard from '../pages/Dashboard'
+
+function Layout() {
+    return (
+        <>
+
+            <div className="layout-wrapper layout-content-navbar">
+                <div className="layout-container">
+                    {/* <!-- Menu --> */}
+
+                    <Sidebar />
+
+                    {/* <!-- Menu --> */}
+                    {/* <!-- Layout container --> */}
+                    <div className="layout-page">
+                        {/* <!-- Navbar --> */}
+
+                        <Navbar />
+
+                        {/* <!-- / Navbar --> */}
+
+                        {/* <!-- Content wrapper --> */}
+                        <div className="content-wrapper">
+                            {/* <!-- Content --> */}
+                                <Dashboard />
+                            {/* <!-- / Content --> */}
+
+                            {/* <!-- Footer --> */}
+                            <Footer />
+                            {/* <!-- / Footer --> */}
+
+                            <div className="content-backdrop fade"></div>
+                        </div>
+                        {/* <!-- Content wrapper --> */}
+                    </div>
+                    {/* <!-- / Layout page --> */}
+                </div>
+
+                {/* <!-- Overlay --> */}
+                <div className="layout-overlay layout-menu-toggle"></div>
+            </div>
+
+        </>
+    )
+}
+
+export default Layout
